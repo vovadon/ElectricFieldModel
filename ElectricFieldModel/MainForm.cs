b6f64a0f277fa74f6cdd006577d06ac1d3e36c07
vovadon <v.y.donchenko@gmail.com>
2018-04-29 11:32:44 +0300

    add MyOl.cs

--- ElectricFieldModel/MainForm.cs
+++ ElectricFieldModel/MainForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
 
 namespace ElectricFieldModel
 {
@@ -15,6 +17,154 @@ namespace ElectricFieldModel
         public MainForm()
         {
             InitializeComponent();
+
+            gCtrl.Load += GCtrl_Load;
+            gCtrl.Paint += GCtrl_Paint;
+            gCtrl.KeyDown += GCtrl_KeyDown;
+
+            rotationTimer.Tick += RotationTimer_Tick;
+            startTimerBut.Click += (o, e) => rotationTimer.Start();
+        }
+
+        private void RotationTimer_Tick(object sender, EventArgs e)
+        {
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.Rotate(2, 0, 0, 1);
+
+            gCtrl.Invalidate();
+        }
+
+        private void GCtrl_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                GL.MatrixMode(MatrixMode.Projection);
+                GL.Rotate(30, 0, 0, 1);
+            }
+            if (e.KeyCode == Keys.B)
+            {
+                GL.MatrixMode(MatrixMode.Modelview);
+                GL.Rotate(30, 0, 0, 1);
+            }
+
+            gCtrl.Invalidate();
+        }
+
+        private void GCtrl_Paint(object sender, PaintEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            #region Cube
+            ///*задняя*/
+            //GL.Color3(Color.Red);
+            //GL.Begin(PrimitiveType.Polygon);
+            //GL.Vertex3(0, 0, 0);
+            //GL.Vertex3(width, 0, 0);
+            //GL.Vertex3(width, width, 0);
+            //GL.Vertex3(0, width, 0);
+            //GL.End();
+
+            ///*левая*/
+            //GL.Begin(PrimitiveType.Polygon);
+            //GL.Vertex3(0, 0, 0);
+            //GL.Vertex3(0, 0, width);
+            //GL.Vertex3(0, width, width);
+            //GL.Vertex3(0, width, 0);
+            //GL.End();
+
+            ///*нижняя*/
+            //GL.Begin(PrimitiveType.Polygon);
+            //GL.Vertex3(0, 0, 0);
+            //GL.Vertex3(0, 0, width);
+            //GL.Vertex3(width, 0, width);
+            //GL.Vertex3(width, 0, 0);
+            //GL.End();
+
+            ///*верхняя*/
+            //GL.Begin(PrimitiveType.Polygon);
+            //GL.Vertex3(0, width, 0);
+            //GL.Vertex3(0, width, width);
+            //GL.Vertex3(width, width, width);
+            //GL.Vertex3(width, width, 0);
+            //GL.End();
+
+            ///*передняя*/
+            //GL.Begin(PrimitiveType.Polygon);
+            //GL.Vertex3(0, 0, width);
+            //GL.Vertex3(width, 0, width);
+            //GL.Vertex3(width, width, width);
+            //GL.Vertex3(0, width, width);
+            //GL.End();
+
+            ///*правая*/
+            //GL.Begin(PrimitiveType.Polygon);
+            //GL.Vertex3(width, 0, 0);
+            //GL.Vertex3(width, 0, width);
+            //GL.Vertex3(width, width, width);
+            //GL.Vertex3(width, width, 0);
+            //GL.End();
+
+            //GL.Color3(Color.Black);
+            //GL.Begin(PrimitiveType.LineLoop);
+            //GL.Vertex3(0, 0, 0);
+            //GL.Vertex3(0, width, 0);
+            //GL.Vertex3(width, width, 0);
+            //GL.Vertex3(width, 0, 0);
+            //GL.End();
+
+            //GL.Begin(PrimitiveType.LineLoop);
+            //GL.Vertex3(width, 0, 0);
+            //GL.Vertex3(width, 0, width);
+            //GL.Vertex3(width, width, width);
+            //GL.Vertex3(width, width, 0);
+            //GL.End();
+
+            //GL.Begin(PrimitiveType.LineLoop);
+            //GL.Vertex3(0, 0, width);
+            //GL.Vertex3(width, 0, width);
+            //GL.Vertex3(width, width, width);
+            //GL.Vertex3(0, width, width);
+            //GL.End();
+
+            //GL.Begin(PrimitiveType.LineLoop);
+            //GL.Vertex3(0, 0, 0);
+            //GL.Vertex3(0, 0, width);
+            //GL.Vertex3(0, width, width);
+            //GL.Vertex3(0, width, 0);
+            //GL.End();
+
+            //GL.Color3(Color.Black);
+            //GL.Begin(PrimitiveType.Lines);
+            //GL.Vertex3(0, 0, 0);
+            //GL.Vertex3(50, 0, 0);
+            //GL.Vertex3(0, 0, 0);
+            //GL.Vertex3(0, 50, 0);
+            //GL.Vertex3(0, 0, 0);
+            //GL.Vertex3(0, 0, 50);
+            //GL.End();
+            #endregion
+
+            GL.Color3(Color.White);
+            
+            MyOl.Sphere(5, 16, 16);
+
+            gCtrl.SwapBuffers();
+        }
+
+        private void GCtrl_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(Color.Black);
+            GL.Enable(EnableCap.DepthTest);
+
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)(60 * Math.PI / 180), 1, 5, 20);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projection);
+
+            Matrix4 modelwiev = Matrix4.LookAt(5, 5, 10, 0, 0, 0, 0, 1, 0);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref modelwiev);
+
+            GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
         }
     }
 }
---
+++ ElectricFieldModel/MyOl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
+
+namespace ElectricFieldModel
+{
+    public static class MyOl
+    {
+        public static void Sphere(double r, int nx, int ny)
+        {
+            int i, ix, iy;
+            double x, y, z;
+
+            for (iy = 0; iy < ny; ++iy)
+            {
+                GL.Begin(PrimitiveType.QuadStrip);
+                for (ix = 0; ix <= nx; ++ix)
+                {
+                    x = r * Math.Sin(iy * Math.PI / ny) * Math.Cos(2 * ix * Math.PI / nx);
+                    y = r * Math.Sin(iy * Math.PI / ny) * Math.Sin(2 * ix * Math.PI / nx);
+                    z = r * Math.Cos(iy * Math.PI / ny);
+                    GL.Normal3(x, y, z);//нормаль направлена от центра
+                    GL.TexCoord2((double)ix / (double)nx, (double)iy / (double)ny);
+                    GL.Vertex3(x, y, z);
+
+                    x = r * Math.Sin((iy + 1) * Math.PI / ny) * Math.Cos(2 * ix * Math.PI / nx);
+                    y = r * Math.Sin((iy + 1) * Math.PI / ny) * Math.Sin(2 * ix * Math.PI / nx);
+                    z = r * Math.Cos((iy + 1) * Math.PI / ny);
+                    GL.Normal3(x, y, z);
+                    GL.TexCoord2((double)ix / (double)nx, (double)(iy + 1) / (double)ny);
+                    GL.Vertex3(x, y, z);
+                }
+                GL.End();
+            }
+        }
+        
+    }
+}
